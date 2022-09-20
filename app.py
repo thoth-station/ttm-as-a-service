eed3bf50b2b3f80c75ec674cac725b309309c8fb
Shreekara <sshreeka@redhat.com>
2022-09-20 15:15:24 -0400

    Migrate from flask_githubapp to gidgethub
    Signed-off-by: Shreekara <sshreeka@redhat.com>

--- app.py
+++ app.py
@@ -1,50 +1,80 @@
+import asyncio
 import os
-
-from flask import Flask
-from flask_githubapp import GitHubApp
+import aiohttp
+from aiohttp import web
+import cachetools
+from gidgethub import aiohttp as gh_aiohttp
+from gidgethub import routing
+from gidgethub import sansio
+from gidgethub.apps import get_installation_access_token
 
 import ttm_github_pr
 
-app = Flask(__name__)
 
-app.config["GITHUBAPP_ID"] = int(os.environ["GITHUBAPP_ID"])
-app.config["GITHUBAPP_SECRET"] = os.environ["GITHUBAPP_SECRET"]
-app.config["GITHUBAPP_KEY"] = os.environ["GITHUBAPP_KEY"]
+GITHUBAPP_ID = os.environ["GITHUBAPP_ID"]
+GITHUBAPP_SECRET = os.environ["GITHUBAPP_SECRET"]
+GITHUBAPP_KEY = os.environ["GITHUBAPP_KEY"]
 
-
-github_app = GitHubApp(app)
-
-
-@app.route("/")
-def root():
-    return "This is a webhook server"
+router = routing.Router()
+cache = cachetools.LRUCache(maxsize=500)
+routes = web.RouteTableDef()
 
 
-@github_app.on("pull_request")
-def process_pr_webhook():
-    # TODO if model exists (training is done and model is published to seldon)
-    # Use predefined location for model i.e. "/{ord/repo}/predict"
+@routes.post("/")
+async def webhook(request):
+    try:
+        body = await request.read()
+        event = sansio.Event.from_http(request.headers, body, secret=GITHUBAPP_SECRET)
+        async with aiohttp.ClientSession() as session:
+            gh = gh_aiohttp.GitHubAPI(session, "ttm-as-a-service", cache=cache)
+            await asyncio.sleep(1)
+            await router.dispatch(event, gh)
+        return web.Response(status=200)
+    except Exception as ex:
+        print(ex)
+        return web.Response(status=500)
+
+
+@router.register("pull_request", action="opened")
+async def opened_pr(event, gh, *arg, **kwargs):
     model_url = "http://thoth-github-ttm-ds-ml-workflows-ws.apps.smaug.na.operate-first.cloud/predict"
+    pull_request = event.data["pull_request"]
+    installation_access_token = await get_installation_access_token(
+        gh,
+        installation_id=event.data["installation"]["id"],
+        app_id=GITHUBAPP_ID,
+        private_key=GITHUBAPP_KEY,
+    )
     prediction = ttm_github_pr.get_ttm(
         model_url,
-        github_app.payload["repository"]["id"],
-        github_app.payload["number"],
-        github_app.installation_token,
+        pull_request["head"]["repo"]["id"],
+        event.data["number"],
+        installation_access_token["token"],
     )
-    pr = github_app.installation_client.pull_request(
-        github_app.payload["repository"]["owner"]["login"],
-        github_app.payload["repository"]["name"],
-        github_app.payload["number"],
+    url = f"/repos/{pull_request['head']['repo']['full_name']}/issues/{event.data['number']}/comments"
+    await gh.post(
+        url,
+        data={
+            "body": f"{prediction}",
+        },
+        oauth_token=installation_access_token["token"],
     )
 
-    pr.create_comment(prediction)
 
-
-@github_app.on("installation.created")
-def install_app():
+@router.register("installation", action="created")
+async def install_app():
     print("installed")
 
 
-@github_app.on("installation.deleted")
-def uninstall_app():
-    print("removed")
+@router.register("installation", action="deleted")
+async def uninstall_app():
+    print("uninstalled")
+
+
+if __name__ == "__main__":
+    app = web.Application()
+    app.router.add_routes(routes)
+    port = os.environ.get("PORT")
+    if port is not None:
+        port = int(port)
+    web.run_app(app, port=port)
